78994cd8c2c347fe1a02451cb8530b3930e763a5
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-11-27 04:07:38 +0300

    Add files via upload

---
+++ 30 kadar  yazdırmak/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _30_kadar__yazdırmak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+            do
+            {
+                richTextBox1.Text += sayac.ToString();
+                sayac++;
+            } while (sayac <= 30);
+        }
+    }
+}
